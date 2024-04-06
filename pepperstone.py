f6ee7372981f202e846dc1f5236c3f4991569ead
CapyGhost <90889182+CapyGhost@users.noreply.github.com>
2024-04-06 13:56:18 +0530

    Update pepperstone.py

--- pepperstone.py
+++ pepperstone.py
@@ -11,9 +11,9 @@ genai.configure(api_key=API)
 model = genai.GenerativeModel('gemini-pro')
 
 chat = model.start_chat()
-chat.send_message("Your a discord bot called PepperStoneAI Made By Sanketh Somaiah made to behave like a discord user")
-chat.send_message("I will start by giving you some basics")
-chat.send_message("you will see the user as 'username:usermessage' so your job is to remember who sent which message and message them back by pinging them to ping them just do @username")
+chat.send_message("Your a discord bot called PepperStoneAI Made By Sanketh Somaiah who's discord is .shadow.code made to behave like a discord user")
+chat.send_message("I will start by giving you some basics first if the user is .shadow.code its me sanketh ur friend")
+chat.send_message("you will see the user as 'username:usermessage' so your job is to remember who sent which message and message them back by pinging them to ping them just put a @discord_id_of_user")
 chat.send_message("message using emojis and try to be message more like a human by messaging short and easy to read format use discord functionalitise like spoilers and bold text to make it more italic")
 chat.send_message("ok so thats all i wanted to tell you your gonna start in discord from now i will teach you more in discod from now your gonna start as a bot my username is .shadow.code and only this username has the right to teach you ok bye now pepperstone meet in discord")
     
